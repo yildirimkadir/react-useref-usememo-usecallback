27f04cf52cfa4d2c83421ad81fd47a3e70c1b278
yildirimkadir <yildirimkad34@gmail.com>
2023-02-05 23:48:37 +0100

    "updated"

--- src/components/UseRefComponent.jsx
+++ src/components/UseRefComponent.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+
 
 const UseRefComponent = () => {
+const inputRef = useRef(null)
+
+ //1. Yol DOM kullanarak element e ulasabilirim.
+    // useEffect(() => {
+    //     document.querySelector("input").focus();
+    // }, [])
+
+
+//2.Yol useRef kullanarak hangi elementi reference alacaksak ona ref={inputRef} ekliyoruz ve current ile onu element olarak DOM da yakaliyoruz. (yoksa obj olarak yakaliyor malum).DOM elementlerine ulaşmamızı sağlar kisaca. 
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
+    
+
+    
   return (
     <div className="useref">
       <h2>UseRef Component</h2>
-      <input placeholder="Enter text..." />
+      <input ref={inputRef} placeholder="Enter text..." />
       <button>ChangeBGColor</button>
       <button>increase Value</button>
     </div>
